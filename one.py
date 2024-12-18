b47d3dd2d5243bfc3becb5069f58a909c50d7964
Jesse Heikkinen <jessehei129@gmail.com>
2024-12-18 13:37:03 +0200

    first commit

---
+++ one.py
@@ -0,0 +1,25 @@
+import logging
+
+logging.basicConfig(
+    format="{asctime} - {levelname} - {message}",
+    style="{",
+    datefmt="%Y-%m-%d %H:%M:%S",
+    level=logging.DEBUG
+)
+input_file = 'input_one.txt'
+left = []
+left_seperated = []
+right = []
+right_seperated = []
+final = []
+seperator = '   '
+with open(input_file) as f:
+    for line in f:
+        left.append(line.split(seperator)[0])
+        right.append(line.split(seperator)[1].strip('\n'))
+
+left = sorted(list(map(int, left)))
+right = sorted(list(map(int,right)))
+
+total_distance = sum(abs(l - r) for l, r in zip(left, right))
+logging.debug(f"TOTAL DISTANCE {total_distance}")
